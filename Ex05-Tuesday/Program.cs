fb01b6b73b35f440def1cf3c7e5260c4cc34bc61
Matthew Anthony Peterson <matt2694@edu.eal.dk>
2017-01-31 13:23:27 +0100

    Add project files.

---
+++ Ex05-Tuesday/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Ex05_Tuesday
+{
+    class Program
+    {
+        private static Random _random = new Random();
+        private static int _temp;
+        private static bool _cont = true;
+        private static int _alarm = 0;
+        static void Main(string[] args)
+        {
+            Thread myThread1 = new Thread(new ThreadStart(Thread1));
+            myThread1.Start();
+            while (myThread1.IsAlive)
+            {
+                Thread.Sleep(10000);
+            }
+            Console.WriteLine("Alarm-wire terminated");
+            Console.ReadKey();
+        }
+        static void Thread1()
+        {
+            while (_cont)
+            {
+                _temp = _random.Next(141) - 20;
+                Console.WriteLine(_temp + "°C");
+                Thread.Sleep(2000);
+                if (_temp < 0 || _temp > 100)
+                {
+                    _alarm++;
+                    Console.WriteLine("Warning: Temperature outside limits!");
+                    Thread.Sleep(2000);
+
+                }
+                if (_alarm >= 3)
+                {
+                    _cont = false;
+                }
+            }
+        }
+    }
+}
